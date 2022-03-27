ee53825eb5dd146210dc6ae947af3bb68a2f9ec1
Danila-Gosden <danilagosden@gmail.com>
2022-03-27 11:12:47 +0200

    The task 12 was completed

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,5 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
-Route::apiResources(['desks' => \App\Http\Controllers\TaskController::class]);
+    Route::prefix("/v1")->apiResources(['tasks' => \App\Http\Controllers\TaskController::class]);
 });
